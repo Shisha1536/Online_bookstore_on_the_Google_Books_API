6f8bf93bbb841ee449ab43c90fc84abb633e103f
Shisha1536 <135944742+Shisha1536@users.noreply.github.com>
2024-01-14 23:02:06 +0300

    conf

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: path.resolve(__dirname, 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'src/js'),
+        filename: 'main.js'
+    },
+    mode: 'development',
+    plugins: [new MiniCssExtractPlugin()],
+    module: {
+        rules: [
+          {
+            test: /\.scss$/i,
+            use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+          },
+        ],
+    },
+}
